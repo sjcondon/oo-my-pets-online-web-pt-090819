a8e3f51b5ac6c25f0c71e4e102773fa14a2a097d
sarah <s.j.condon01@gmail.com>
2019-10-15 00:12:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,15 @@
 class Dog
   attr_reader :name
-  attr_accessor :mood
+  attr_accessor :mood, :owner
 
-  def initialize(name)
+  def initialize(name,owner)
     @name = name
+    @owner = owner
     @mood = "nervous"
+    @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
 end
