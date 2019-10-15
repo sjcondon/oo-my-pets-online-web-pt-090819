bc699da82849b30a596323304e4e64bea970e7c5
sarah <s.j.condon01@gmail.com>
2019-10-15 00:06:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,28 +51,22 @@ class Owner
 
 
   def pets
-    @@pets
+    pets = dogs + cats
   end
 
-
-#   def play_with_cats
-#     @@pets[:cats].each do |cat|
-#       cat.mood = 'happy'
-#     end
-#   end
+    def sell_pets
+    pets.each do |pet, arr|
+      arr.map do |pet|
+        pet.mood = 'nervous'
+      end
+      arr.clear
+    end
+  end
 
  
 #   def list_pets
 #     "I have  #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
 #   end
 
-#   def sell_pets
-#     pets.each do |pet, arr|
-#       arr.map do |pet|
-#         pet.mood = 'nervous'
-#       end
-#       arr.clear
-#     end
-#   end
 
 end
